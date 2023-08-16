42b3bd18cf2be21a327f03b81cd7390cd3a7ec55
CuongDo <doduccuong2001@gmail.com>
2023-08-16 16:19:54 +0700

    Add: test seo page

---
+++ src/app/head.tsx
@@ -0,0 +1,28 @@
+export default function Head() {
+  return (
+    <>
+      <title>
+        Đại lý cám bảo thuỳ - mua hàng tốt, hàng chuẩn, ship tận nơi
+      </title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="Siêu thị cám chuyên cung cấp thức ăn chăn nuôi"
+      />
+      <link rel="icon" href="/favicon.ico" />
+      <meta
+        property="og:title"
+        content="Đại lý cám bảo thuỳ - mua hàng tốt, hàng chuẩn, ship tận nơi"
+      />
+      <meta
+        property="og:description"
+        content="Siêu thị cám chuyên cung cấp thức ăn chăn nuôi"
+      />
+      <meta property="og:image" content="/img/logo-cam.png" />
+      <meta
+        property="og:url"
+        content="https://sieuthicam-baothuy.vercel.app/"
+      />
+    </>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import { Header } from "@/components/Header";
 import "./globals.css";
-import type { Metadata } from "next";
+
 import { Inter } from "next/font/google";
 import { Footer } from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Đại lý cám bảo thuỳ - mua hàng tốt, hàng chuẩn, ship tận nơi",
-  description: "Siêu thị cám chuyên cung cấp thức ăn chăn nuôi",
-};
 
 export default function RootLayout({
   children,
@@ -18,6 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head />
       <body className={inter.className}>
         <Header />
         <div className="min-h-screen bg-accent">{children}</div>
